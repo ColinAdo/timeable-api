f62f902443ae4c0fe3149c5a82c6637fe93227ed
ColinAdo <colinadodev30@gmail.com>
2024-05-25 17:33:29 +0300

    Add Custom jwt urls

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.jwt')),
     path('api/v1/', include('auths.urls'))
 ]
