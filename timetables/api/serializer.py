7847d90cb6b3891242b8d590179b8afde19ab4f1
ColinAdo <colinadodev30@gmail.com>
2024-05-30 13:40:12 +0300

    Add timetable and timetable names serializer

--- timetables/api/serializer.py
+++ timetables/api/serializer.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 
-from timetables.models import Timetable
+from timetables.models import Timetable, TimetableName
 
 class TimetableSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Timetable
         fields = '__all__'
+
+class TimetableNameSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    timetable = TimetableSerializer()
+
+    class Meta:
+        model = TimetableName
+        fields = (
+            'id', 
+            'name',
+            'user',
+            'timetable',
+            'created',
+        )
