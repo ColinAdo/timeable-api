e6d1dfac95478326e60cdb0f0281d6443d6015f1
ColinAdo <colinadodev30@gmail.com>
2024-05-30 10:27:04 +0300

    Add fields to model properly

--- timetables/models.py
+++ timetables/models.py
@@ -3,18 +3,18 @@ from django.db import models
 
 
 class Timetable(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     day = models.CharField(max_length=10)
-    unit_code = models.CharField(max_length=10, null=True)
-    unit_name = models.CharField(max_length=200, null=True)
-    start_time = models.TimeField(null=True)
-    end_time = models.TimeField(null=True)
+    unit_code = models.CharField(max_length=10)
+    unit_name = models.CharField(max_length=200)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
     lecturer = models.CharField(max_length=200, null=True, blank=True)
     campus = models.CharField(max_length=200, null=True, blank=True)
     mode_of_study = models.CharField(max_length=200, null=True, blank=True)
     lecture_room = models.CharField(max_length=200, null=True, blank=True)
     group = models.IntegerField(default=0, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('day',)
