32c8c5930ef474e506ab302706e3d67a70e5ce4b
ColinAdo <colinadodev30@gmail.com>
2025-01-08 15:25:15 +0300

    Removed unwanted import

--- timetables/api/urls.py
+++ timetables/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from rest_framework.routers import DefaultRouter
-
 from django.urls import path
 from .views import UploadUnitsView, GenerateTimetableView
 
@@ -9,22 +7,3 @@ urlpatterns = [
     path('upload_units/', UploadUnitsView.as_view(), name='upload_units'),
     path('generate_timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
 ]
-
-
-# from .views import (
-#     TimetableViewset, 
-#     UnitUploadView, 
-#     TimetableNameViewset, 
-#     TimetableDetailView
-# )
-
-# router = DefaultRouter()
-
-# urlpatterns = [
-#     path('upload/', UnitUploadView.as_view(), name='upload'),
-#     path('timetabledetail/<str:pk>/', TimetableDetailView.as_view(), name='detail'),
-# ]
-
-# router.register(r'timetable', TimetableViewset, basename='timetable')
-# router.register(r'timetablenames', TimetableNameViewset, basename='timetablenames')
-# urlpatterns += router.urls
