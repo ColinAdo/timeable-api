eb76112ce96008fca7e3aabb19e108e1583f25ee
ColinAdo <colinadodev30@gmail.com>
2025-01-08 15:26:27 +0300

    Removed unwanted comment

--- timetables/api/serializer.py
+++ timetables/api/serializer.py
@@ -12,28 +12,3 @@ class TimetableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timetable
         fields = '__all__'
-
-
-# from timetables.models import Timetable, TimetableName
-
-# # Timetable serializer
-# class TimetableSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Timetable
-#         fields = '__all__'
-
-
-# # Timetable names serializer
-# class TimetableNameSerializer(serializers.ModelSerializer):
-#     user = serializers.ReadOnlyField(source='user.username')
-
-#     class Meta:
-#         model = TimetableName
-#         fields = (
-#             'id',
-#             'name',
-#             'user',
-#             'timetable',
-#             'created_at',
-#         )
