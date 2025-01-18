efd1d8da87a96fe99949789ee5242aa5501f552c
ColinAdo <colinadodev30@gmail.com>
2025-01-18 12:50:41 +0300

    Add export imetable url

--- timetables/api/urls.py
+++ timetables/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UploadUnitsView, GenerateTimetableView
+from .views import UploadUnitsView, GenerateTimetableView, ExportTimetableView
 
 urlpatterns = [
     path('upload_units/', UploadUnitsView.as_view(), name='upload_units'),
     path('generate_timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
+    path('export_timetable/', ExportTimetableView.as_view(), name='export_timetable'),
 ]
