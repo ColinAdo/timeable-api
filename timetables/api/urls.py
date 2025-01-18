faf0f6336d4ebfa1c59b045072ea25d85f43b25f
ColinAdo <colinadodev30@gmail.com>
2025-01-18 14:06:58 +0300

    Add url endpoint

--- timetables/api/urls.py
+++ timetables/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import UploadUnitsView, GenerateTimetableView, ExportTimetableView
+from .views import (
+    UploadUnitsView, 
+    GenerateTimetableView, 
+    ExportTimetableView,
+    SendTimetableEmailView
+)
 
 urlpatterns = [
     path('upload_units/', UploadUnitsView.as_view(), name='upload_units'),
     path('generate_timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
     path('export_timetable/', ExportTimetableView.as_view(), name='export_timetable'),
+    path('export_timetable_email/', SendTimetableEmailView.as_view(), name='export_timetable_email'),
 ]
