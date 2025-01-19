6edf18db93a71eb5610649d8363db1138107f48b
ColinAdo <colinadodev30@gmail.com>
2025-01-19 16:22:32 +0300

    Add proper uls Conf

--- timetables/api/urls.py
+++ timetables/api/urls.py
@@ -8,8 +8,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('upload_units/', UploadUnitsView.as_view(), name='upload_units'),
-    path('generate_timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
-    path('export_timetable/', ExportTimetableView.as_view(), name='export_timetable'),
-    path('export_timetable_email/', SendTimetableEmailView.as_view(), name='export_timetable_email'),
+    path('upload/units/', UploadUnitsView.as_view(), name='upload_units'),
+    path('generate/timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
+    path('export/timetable/', ExportTimetableView.as_view(), name='export_timetable'),
+    path('export/timetable/email/', SendTimetableEmailView.as_view(), name='export_timetable_email'),
 ]
