8e23cbaf59bfe57c411b7d40adf82b319864f394
ColinAdo <colinadodev30@gmail.com>
2025-01-19 16:36:54 +0300

    Add proper import

--- auths/views.py
+++ auths/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from djoser.social.views import ProviderAuthView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
