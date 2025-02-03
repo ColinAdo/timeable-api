39d089eee0e3cd3206a15a650f2432f187a06bff
ColinAdo <colinadodev30@gmail.com>
2025-02-03 09:51:55 +0300

    Add subscription model

--- timetables/models.py
+++ timetables/models.py
@@ -28,3 +28,14 @@ class Timetable(models.Model):
 
 #     def __str__(self):
 #         return f'{self.user.username} timetable'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
+    tier = models.CharField(max_length=10)
+    status = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} Subscriptions'
