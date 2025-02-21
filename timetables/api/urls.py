d1655e9a4b0f5a9ff8a36ebb8e59148f546c13ac
ColinAdo <colinadodev30@gmail.com>
2025-02-21 17:18:26 +0300

    Add timetable names url

--- timetables/api/urls.py
+++ timetables/api/urls.py
@@ -5,10 +5,12 @@ from .views import (
     UploadUnitsView, 
     ExportToEmailView,
     GenerateTimetableView, 
+    TimetableNameView,
 )
 
 urlpatterns = [
     path('upload/units/', UploadUnitsView.as_view(), name='upload_units'),
+    path('timetable/names/', TimetableNameView.as_view(), name='timetable_name'),
     path('generate/timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
     path('export/timetable/', ExportToEmailView.as_view(), name='export_timetable_email'),
     path('timetable/<str:batch_id>/', TimetableView.as_view(), name='timetable'),
