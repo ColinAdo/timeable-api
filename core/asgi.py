143ef168f6430a64d7a8b74268f8bbed2734fee9
ColinAdo <colinadodev30@gmail.com>
2025-03-07 17:54:16 +0300

    Add proper asgi code

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,21 @@
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
+from channels.security.websocket import AllowedHostsOriginValidator # type: ignore
 from django.core.asgi import get_asgi_application
+from timetables import routing
+from timetables.middleware.jwt_auth_middleware import JWTAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(routing.websocket_urlpatterns)
+        )
+    ),
+})
