0111e88f18e2917d3794225d713ad32837971ac3
ColinAdo <colinadodev30@gmail.com>
2025-03-25 14:26:14 +0300

    Add signals file

--- timetables/apps.py
+++ timetables/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TimetablesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'timetables'
+
+    def ready(self):
+        import timetables.signals
