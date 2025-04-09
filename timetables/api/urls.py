5cd0c09e0b18ad816682406a8df8d0aa67a662f9
ColinAdo <colinadodev30@gmail.com>
2025-04-09 11:19:42 +0300

    Add proper code structure

--- timetables/api/urls.py
+++ timetables/api/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('user/subscription/', SubscriptionView.as_view(), name='subscription'),
     path('timetable/names/', TimetableNameView.as_view(), name='timetable_name'),
     path('timetable/<str:batch_id>/', TimetableView.as_view(), name='timetable'),
-    path('generate/timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
     path('export/timetable/', ExportToEmailView.as_view(), name='export_timetable_email'),
     path('timetable/get/<str:rowId>/', TimetableDataView.as_view(), name='timetable_data'),
+    path('generate/timetable/', GenerateTimetableView.as_view(), name='generate_timetable'),
 ]
